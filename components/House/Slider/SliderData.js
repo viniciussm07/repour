a3e0ee86abf4e9b7371503f7d4487c4e3a2f48ab
viniciussm07 <vini.soares.martins@gmail.com>
2024-01-25 02:21:40 -0300

    Adding house room swiper

--- components/House/Slider/SliderData.js
+++ components/House/Slider/SliderData.js
@@ -1,13 +1,88 @@
-import img1 from '../../../public/house/sala-estudos.jpg'
-import img2 from '../../../public/house/sala-tv.jpg'
+import img1 from '../../../public/house/dogs.jpg'
+import img2 from '../../../public/house/sala-sinuca.JPG'
+import img3 from '../../../public/house/frente.jpg'
+import img4 from '../../../public/house/sala-tv.jpg'
+import img5 from '../../../public/house/fundo.jpg'
+import img6 from '../../../public/house/sala-estudos.jpg'
+import img7 from '../../../public/house/suite.jpg'
+import img8 from '../../../public/house/banheiro-suite.jpg'
+import img9 from '../../../public/house/quarto-1.jpg'
+import img10 from '../../../public/house/quarto-2.jpg'
+import img11 from '../../../public/house/quarto-3.jpg'
+import img12 from '../../../public/house/banheiro-social.jpg'
+import img13 from '../../../public/house/quarto-fundo.jpg'
+import img14 from '../../../public/house/lavabo-fundo.jpg'
+import img15 from '../../../public/house/banheiro-fundo.jpg'
+import img16 from '../../../public/house/cozinha.jpg'
+import img17 from '../../../public/house/garagem.jpg'
 
 export const SliderData = [
     {
-        title: "Sala de Estudos",
+        title: "Cachorras",
         image: img1
     },
     {
         title: "Sala de TV",
         image: img2
+    },
+    {
+        title: "Frente",
+        image: img3
+    },
+    {
+        title: "Sala de TV",
+        image: img4
+    },
+    {
+        title: "Fundo",
+        image: img5
+    },
+    {
+        title: "Sala de Estudos",
+        image: img6
+    },
+    {
+        title: "Suite",
+        image: img7
+    },
+    {
+        title: "Banheiro da Suite",
+        image: img8
+    },
+    {
+        title: "Quarto 1",
+        image: img9
+    },
+    {
+        title: "Quarto 2",
+        image: img10
+    },
+    {
+        title: "Quarto 3",
+        image: img11
+    },
+    {
+        title: "Banheiro Social",
+        image: img12
+    },
+    {
+        title: "Quarto Fundo",
+        image: img13
+    },
+    {
+        title: "Lavabo Fundo",
+        image: img14
+    },
+    {
+        title: "Banheiro Fundo",
+        image: img15
+    },
+    {
+        title: "Cozinha",
+        image: img16
+    },
+    {
+        title: "Garagem",
+        image: img17
     }
 ]
---
+++ components/House/Slider/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Pagination } from "swiper/modules";
+import { SliderData } from "./SliderData";
+import Image from "next/image";
+import {RxArrowLeftSLine, RxArrowRightSLine} from "react-icons/rx";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/free-mode";
+
+function Slider() {
+  const slides = SliderData;
+  return (
+    <div className="flex items-center justify-center flex-col h-screen ">
+      <Swiper
+        breakpoints={{
+          340: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          700: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          }
+        }}
+        modules={[FreeMode, Pagination]}
+        freeMode={true}
+        pagination={{clickable: true}}
+        className="max-w-7xl w-full h-full"
+      >
+        {slides.map((slide) => (
+          <SwiperSlide key={slide.title}>
+            <Image src={slide.image} alt={slide.title} />
+            <div className="flex items-center justify-center flex-col">
+              <h3 className="text-3xl py-6">{slide.title}</h3>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
+
+export default Slider;
