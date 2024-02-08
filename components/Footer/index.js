8c598d04cd97ed0de77a6595cf5d5b3c908b9b95
viniciussm07 <vini.soares.martins@gmail.com>
2024-02-08 16:42:28 -0300

    Fixing Footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,21 +1,101 @@
-import React from "react";
 import Image from "next/image";
+import React from "react";
+import logo from "../../public/logos/logo.webp";
+import { Link as Links } from "react-scroll";
 
 export default function Footer() {
   return (
-    <section className="flex items-center justify-center bg-neutral-800">
-      <div className="flex-1 mt-4">
-      </div>
-      <div className="flex-1 m-16 text-red-700">
-        <div className="text-xl">
-          <h2 className="text-white">Contacts</h2>
-          <ul className="">
-            <li className="text-gray-300">Email: ourrep019@gmail.com</li>
-            <li className="text-gray-300">Phone: (19) 99812-7987</li>
-            <li className="text-gray-300">Address: 123 Main St, City, State</li>
+    <footer id="contato" className="bg-neutral-800">
+      <div className="w-full max-w-5xl mx-auto p-6 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <Links
+            href="#home"
+            to="home"
+            smooth={true}
+            duration={600}
+            spy={true}
+            exact="true"
+            offset={-100}
+            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+          >
+            <Image
+              src={logo}
+              className="h-20 w-20 mr-3"
+              alt="República Our Logo"
+            />
+          </Links>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0">
+            <li className="m-2">
+              <Links
+                href="#quem-somos"
+                to="quem-somos"
+                smooth={true}
+                duration={600}
+                spy={true}
+                exact="true"
+                offset={-135}
+                className="hover:text-yellow-400"
+              >
+                Quem Somos
+              </Links>
+            </li>
+            <li className="m-2">
+              <Links
+                href="#infos"
+                to="infos"
+                smooth={true}
+                duration={600}
+                spy={true}
+                exact="true"
+                offset={-135}
+                className="hover:text-yellow-400"
+              >
+                Infos
+              </Links>
+            </li>
+            <li className="m-2">
+              <Links
+                href="#nossa-casa"
+                to="nossa-casa"
+                smooth={true}
+                duration={600}
+                spy={true}
+                exact="true"
+                offset={-135}
+                className="hover:text-yellow-400"
+              >
+                Nossa Casa
+              </Links>
+            </li>
+            <li className="m-2">
+              <Links
+                href="#moradores"
+                to="moradores"
+                smooth={true}
+                duration={600}
+                spy={true}
+                exact="true"
+                offset={-135}
+                className="hover:text-yellow-400"
+              >
+                Moradores
+              </Links>
+            </li>
           </ul>
+          <div className="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0">
+            Contato: (19) 99812-7987
+          </div>
         </div>
+
+        <hr className="my-6 border-neutral-500 sm:mx-auto  lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2024{" "}
+          <a href="https://repour.vercel.app/" className="hover:underline">
+            Rep Our
+          </a>
+          . All Rights Reserved.
+        </span>
       </div>
-    </section>
+    </footer>
   );
 }
