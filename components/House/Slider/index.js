0a81ae6c802c1ad0a65f46a12b361256b1328767
viniciussm07 <vini.soares.martins@gmail.com>
2024-02-08 19:01:13 -0300

    Fixing slider

--- components/House/Slider/index.js
+++ components/House/Slider/index.js
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode, Pagination } from "swiper/modules";
 import { SliderData } from "./SliderData";
 import Image from "next/image";
-import {RxArrowLeftSLine, RxArrowRightSLine} from "react-icons/rx";
+import { RxArrowLeftSLine, RxArrowRightSLine } from "react-icons/rx";
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -14,6 +14,10 @@ function Slider() {
   return (
     <div className="flex items-center justify-center flex-col">
       <Swiper
+        style={{
+          "--swiper-navigation-color": "#B91C1C",
+          "--swiper-pagination-color": "#B91C1C",
+        }}
         breakpoints={{
           340: {
             slidesPerView: 1,
@@ -21,19 +25,19 @@ function Slider() {
           },
           700: {
             slidesPerView: 2,
-            spaceBetween: 40,
-          }
+            spaceBetween: 35,
+          },
         }}
         modules={[FreeMode, Pagination]}
         freeMode={true}
-        pagination={{clickable: true}}
-        className="max-w-7xl w-full"
+        pagination={{ clickable: true }}
+        className="w-screen z-10"
       >
         {slides.map((slide) => (
           <SwiperSlide key={slide.title}>
             <Image src={slide.image} alt={slide.title} />
             <div className="flex items-center justify-center flex-col">
-              <h3 className="text-3xl py-6">{slide.title}</h3>
+              <h3 className="text-3xl m-8 py-6">{slide.title}</h3>
             </div>
           </SwiperSlide>
         ))}
