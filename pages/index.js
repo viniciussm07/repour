2287600dbf06a2435766480df3a0f133295ddd10
viniciussm07 <vini.soares.martins@gmail.com>
2024-02-09 12:06:42 -0300

    Adding meta tags best pratices SEO

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,27 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Rep Our</title>
         <meta
           name="description"
           content="República estudantil de São Carlos com estudantes da USP e UFSCar.
 Venha vivenciar a melhor experiência da sua vida!"
         />
+        <meta property="og:title" content="República Our - São Carlos" />
+        <meta property="og:site_name" content="República Our - São Carlos" />
+        <meta
+          property="og:description"
+          content="República estudantil de São Carlos com estudantes da USP e UFSCar.
+          Venha vivenciar a melhor experiência da sua vida!"
+        />
+        <meta
+          property="og:image"
+          content="https://repour.vercel.app/logos/logo.webp"
+        />
+        <meta
+          property="og:url"
+          content="https://repour.vercel.app/"
+        />
+        <title>República Our - São Carlos</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon/favicon.ico" />
       </Head>
