c95172372123f8ce9a5fae5099890b2e4623e1e7
viniciussm07 <vini.soares.martins@gmail.com>
2024-02-10 18:44:42 -0300

    Fix

--- components/House/index.js
+++ components/House/index.js
@@ -5,7 +5,7 @@ const House = () => {
   return (
     <section
       id="nossa-casa"
-      className="flex flex-col items-center py-10 justify-center text-white bg-neutral-800 text-white w-full h-auto"
+      className="flex flex-col items-center py-10 justify-center text-white bg-neutral-800 w-full h-auto"
     >
       <h2 className="text-3xl md:text-4xl py-5">Nossa Casa</h2>
       <div className="w-full flex flex-col space-x-4 py-10">
