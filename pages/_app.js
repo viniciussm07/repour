ec5af223bf86f298702520653e64267b046d040b
viniciussm07 <vini.soares.martins@gmail.com>
2024-02-12 14:49:03 -0300

    Adding google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
 
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
+import * as gtag from "/lib/gtag";
+
 export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    TagManager.initialize({ gtmId: gtag.GA_TRACKING_ID });
+  }, []);
+
   return <Component {...pageProps} />
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,23 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from "/lib/gtag";
 
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head />
+      <Head>
+      <meta
+          name="google-site-verification"
+          content="7RPTkb9XB6qmjoHvD8cvFMGLjMf2Ew4_pS8LaTnlaRk"
+        />
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GA_TRACKING_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+      </Head>
       <body className='scrollbar'>
         <Main />
         <NextScript />
