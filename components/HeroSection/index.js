39853e6b0e030a48347bfc2f4929a4402353675b
viniciussm07 <vini.soares.martins@gmail.com>
2024-02-12 14:18:27 -0300

    Changing Hero Section background and adding drop-shadow in h1

--- components/HeroSection/index.js
+++ components/HeroSection/index.js
@@ -7,13 +7,13 @@ export default function HeroSection() {
   return (
     <section
       id="home"
-      className="h-screen md:flex md:items-center md:justify-center bg-cover bg-no-repeat bg-top md:bg-fixed bg-neutral-800 bg-[url('/our-photos/churrasco-dos-pais-teste.jpg')] md:bg-[url('/our-photos/reencontrOUR.jpeg')] tracking-wide"
+      className="h-screen md:flex md:items-center md:justify-center bg-cover bg-no-repeat bg-top md:bg-fixed bg-neutral-800 bg-[url('/our-photos/churrasco-dos-pais-2024.webp')] md:bg-[url('/our-photos/reencontrOUR.jpeg')] tracking-wide"
     >
       <div className="pt-28 flex items-center justify-center flex-col">
         <div className="w-20 h-20 md:w-80 md:h-80">
           <Image width={400} src={logo} alt="A República OUR" />
         </div>
-        <h1 className="text-yellow-400 text-2xl md:text-3xl pt-4 ">
+        <h1 className="text-yellow-400 text-2xl md:text-3xl pt-4 drop-shadow-3xl">
           República Our - São Carlos SP
         </h1>
       </div>
